b29f195a4ca70f2a23eabc046cb5b69fda858168
Yong-L <dkrcjs7521@gmail.com>
2018-04-23 14:20:39 -0400

    Server rewrite

--- server/server.go
+++ server/server.go
@@ -1,57 +1,105 @@
-// Server to host GedditQL Database. Will default to port 3306
-
-package main
+// Simple TCP Server for GedditQL
+package tcpserver
 
 import (
 	"bufio"
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
-const (
-	// ConnHost is for hosting localhost
-	ConnHost = "localhost"
-	// ConnPort is the port for the database server
-	ConnPort = "3306"
-	// ConnType is the type of connection that the db server will accept
-	ConnType = "tcp"
-)
-
-func main() {
-	// Listen for incoming connections
-	l, err := net.Listen(ConnType, ConnHost+":"+ConnPort)
-	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
-	}
-	// Close listener when application closes
-	defer l.Close()
-
-	fmt.Println("Listening on " + ConnHost + ":" + ConnPort)
-
-	// Loop until database is killed
-	for {
-		// Listen for incoming connections
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting:", err.Error())
-			os.Exit(1)
-		}
-		go handleRequest(conn)
-	}
-
+// Client of each connections
+type Client struct {
+	conn   net.Conn
+	Server *server
 }
 
-func handleRequest(conn net.Conn) {
-	// Read the incoming data to buffer
-	query, err := bufio.NewReader(conn).ReadString(';')
-	// the query must have a semicolon as delimiter
-	fmt.Println(query)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-		os.Exit(1)
+// Server information
+type server struct {
+	address                  string
+	onNewClientCallback      func(c *Client)
+	onClientConnectionClosed func(c *Client, err error)
+	onNewMessage             func(c *Client, message string)
+}
+
+// Listen indefinitely until client closes connection
+func (c *Client) listen() {
+	reader := bufio.NewReader(c.conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			c.conn.Close()
+			c.Server.onClientConnectionClosed(c, err)
+			return
+		}
+		c.Server.onNewMessage(c, message)
 	}
-	conn.Write([]byte("Message received."))
-	conn.Close()
+}
+
+// Send message back to client
+func (c *Client) Send(message string) error {
+	_, err := c.conn.Write([]byte(message))
+	return err
+}
+
+// Get the connection
+func (c *Client) Conn() net.Conn {
+	return c.conn
+}
+
+// Close connection
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+// Called right after server starts listening new client
+func (s *server) OnNewClient(callback func(c *Client)) {
+	s.onNewClientCallback = callback
+}
+
+// Called right after connection closed
+func (s *server) OnClientConnectionClosed(callback func(c *Client, err error)) {
+	s.onClientConnectionClosed = callback
+}
+
+// Called when Client receives new message
+func (s *server) OnNewMessage(callback func(c *Client, message string)) {
+	s.onNewMessage = callback
+}
+
+// Start network server that accepts connections
+func (s *server) Listen() {
+	listener, err := net.Listen("tcp", s.address)
+	if err != nil {
+		log.Fatal("Error starting TCP server.")
+	}
+
+	// Close connection once all other functions end
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Error listening on connection")
+		}
+		client := &Client{
+			conn:   conn,
+			Server: s,
+		}
+		go client.listen()
+		s.onNewClientCallback(client)
+	}
+}
+
+// Creates new tcp server instance
+func New(address string) *server {
+	log.Println("Creating server with address", address)
+	server := &server{
+		address: address,
+	}
+
+	server.OnNewClient(func(c *Client) {})
+	server.OnNewMessage(func(c *Client, message string) {})
+	server.OnClientConnectionClosed(func(c *Client, err error) {})
+
+	return server
 }
