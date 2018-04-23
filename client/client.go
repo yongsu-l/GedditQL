5707bb825f6e0df3d72d386767fb02f9e37f5bbb
Yong-L <dkrcjs7521@gmail.com>
2018-04-23 14:07:43 -0400

    client application for GedditQL

--- client/client.go
+++ client/client.go
@@ -10,11 +10,18 @@ import (
 func main() {
 
 	// Connect to the socket
-	conn, err := net.Dial("tcp", "127.0.0.1:3306")
+	conn, err := net.Dial("tcp", "127.0.0.1:9999")
 	if err != nil {
 		fmt.Println("Error establishing connection:", err.Error())
 		os.Exit(1)
 	}
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(message)
+
 	for {
 		// Read input from stdin
 		reader := bufio.NewReader(os.Stdin)
@@ -24,8 +31,12 @@ func main() {
 			fmt.Println("Error reading string:", err.Error())
 			os.Exit(1)
 		}
+		if query == "quit\n" {
+			fmt.Println("Quitting...")
+			os.Exit(0)
+		}
 		// Send to db server
-		fmt.Fprintf(conn, query)
+		fmt.Fprintf(conn, query+"\n")
 		// Listen for reply
 		message, err := bufio.NewReader(conn).ReadString('\n')
 
