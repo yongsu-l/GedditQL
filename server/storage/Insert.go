bc8d8e15bbbaaf3d7e08e92de3d285892e945907
Yong-L <dkrcjs7521@gmail.com>
2018-04-29 12:47:17 -0400

    Insert

---
+++ server/storage/Insert.go
@@ -0,0 +1,46 @@
+package storage
+
+import "GedditQL/server/interpreter"
+
+// Insert inserts into the table specified
+func (db *Database) Insert(opts interpreter.InsertOptions) (Response, error) {
+
+	t := Response{}
+	var length int
+
+	if _, exists := db.Tables[opts.TableRef]; exists {
+		for _, v := range opts.ColumnRefs {
+			// Check if column name exists in table
+			if _, exists := db.Tables[opts.TableRef].Rows[v]; exists {
+
+				// Append new value at the end
+				db.Tables[opts.TableRef].Rows[v].Data = append(db.Tables[opts.TableRef].Rows[v].Data, v)
+				// Measure length for normalize at end of loop
+				if len(db.Tables[opts.TableRef].Rows[v].Data) > length {
+					length = len(db.Tables[opts.TableRef].Rows[v].Data)
+				}
+			} else {
+				return Response{}, &errorString{"Column name not in database"}
+			}
+		}
+	} else {
+		// Spit error if table doesn't exist in db
+		return Response{}, &errorString{"Table doesn't exist in db"}
+	}
+
+	db.normalize(opts.TableRef, length)
+
+	return t, nil
+}
+
+// Normalize makes it so that if there are no values that were inserted into the db, it will insert an nil value appropriate
+func (db *Database) normalize(tblName string, length int) error {
+
+	for k := range db.Tables[tblName].Rows {
+		if len(db.Tables[tblName].Rows[k].Data) < length {
+			db.Tables[tblName].Rows[k].Data = append(db.Tables[tblName].Rows[k].Data, "")
+		}
+	}
+
+	return nil
+}
